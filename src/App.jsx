c20d828ee8edaf07744286e07a4a8a7af77c0998
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2025-05-23 12:25:30 -0300

    lazy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, lazy, Suspense } from 'react'
 import './App.css'
 import Map from './features/map/Map'
 import Filter from './features/filter/Filter'
-import ContextMenu from './features/context_menu/ContextMenu'
+
 import LayerControl from './features/layer_control/LayerControl'
 import TimeSelector from './features/time_selector/TimeSelector'
-import ModalChart from './features/modal_chart/ModalChart'
 import Legend from './features/legend/Legend'
-import SideChart from './features/sideChart/SideChart'
+
 import { ToastContainer } from 'react-toastify'
 import Loader from './features/loader/Loader'
-import Timeline from './features/timeline/Timeline'
 import Topleft from './features/topleft/Topleft'
 import TopRight from './features/topright/TopRight'
-import TopLoader from './features/loader/top_loader/TopLoader'
+
+const ModalChart = lazy(_=> import('./features/modal_chart/ModalChart'))
+const Timeline = lazy(_=> import('./features/timeline/Timeline'))
+const SideChart = lazy(_=> import('./features/sideChart/SideChart'))
+const TopLoader = lazy(_=> import('./features/loader/top_loader/TopLoader'))
+const ContextMenu = lazy(_=> import('./features/context_menu/ContextMenu'))
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,23 +25,30 @@ function App() {
     <>
       <Filter/>
       <Map/>
-      <ContextMenu/>
+      
       <LayerControl/>
       <TimeSelector/>
-      <ModalChart/>
+      
       <Legend/>
-      <ToastContainer
+      
+      <Loader/>
+      <Suspense fallback={null}>
+        <Timeline/>
+        <ToastContainer
           position="bottom-right"
           autoClose={2000}
           pauseOnFocusLoss={false}
           theme="dark"
-      /> 
-      <Loader/>
-      <Timeline/>
+        /> 
+        <ModalChart/>
+        <SideChart/>
+        <TopLoader/>
+        <ContextMenu/>
+      </Suspense>
+      
       <Topleft/>
       <TopRight/>
-      <SideChart/>
-      <TopLoader/>
+      
     </>
   )
 }
