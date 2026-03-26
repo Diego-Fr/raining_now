186987413452a28ab51612dc12430cd9998be1cd
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2026-03-26 13:50:39 -0300

    ajustes responsividade

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,13 +2,13 @@ import { test } from '@playwright/test';
 
 const sizes = [
   { name: 'mobile', width: 375, height: 667 },
-  // { name: 'tablet', width: 768, height: 1024 },
-  // { name: 'desktop', width: 1280, height: 800 },
-  // { name: 'fullhd', width: 1920, height: 1080 },
-  // { name: 'tv_hd', width: 1280, height: 720 },
-  // { name: 'tv_1366', width: 1366, height: 768 },
-  // { name: 'tv_fullhd', width: 1920, height: 1080 },
-  // { name: 'tv_4k', width: 3840, height: 2160 },
+  { name: 'tablet', width: 768, height: 1024 },
+  { name: 'desktop', width: 1280, height: 800 },
+  { name: 'fullhd', width: 1920, height: 1080 },
+  { name: 'tv_hd', width: 1280, height: 720 },
+  { name: 'tv_1366', width: 1366, height: 768 },
+  { name: 'tv_fullhd', width: 1920, height: 1080 },
+  { name: 'tv_4k', width: 3840, height: 2160 },
 ];
 
 test('screenshots responsivos', async ({ page }) => {
