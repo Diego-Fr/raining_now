67d35c896eb32ca03e0fc1c1b17a6b87e3b6aa23
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2026-04-09 12:22:21 -0300

    ajuste legenda

--- src/features/panels/bottomleft/BottomLeft.jsx
+++ src/features/panels/bottomleft/BottomLeft.jsx
@@ -1,11 +1,14 @@
+import { useIsMobile } from '../../../hooks/isMobile'
 import styles from './BottomLeft.module.scss'
 import GlobalLegend from './GlobalLegend'
 
-const BottomLeft = _ =>{   
+const BottomLeft = _ =>{ 
+    const isMobile = useIsMobile()  
     return (
         <div className={styles.container}>
             <div className={styles.logo}>
-                <GlobalLegend/>
+                {!isMobile && <GlobalLegend/>}
+                
             </div>
         </div>
     )
