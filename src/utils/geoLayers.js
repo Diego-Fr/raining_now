20115394cb18e4466020f089f06a8f3673447095
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2026-09-08 15:41:28 -0300

    ajustando link da camada muni no ppdc

--- src/utils/geoLayers.js
+++ src/utils/geoLayers.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const getGeoCities = async () =>{
     let res = await axios.request({
         method: 'GET',
-        url: 'https://geodados.daee.sp.gov.br/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode%3Amunicipios_sp_simplify&maxFeatures=1000&outputFormat=application%2Fjson'
+        url: 'https://geodados.daee.sp.gov.br/geoserver/sibh/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sibh%3Amunicipios_sp&outputFormat=application%2Fjson&maxFeatures=5000'
     })
 
     return res.data
